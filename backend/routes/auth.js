3c788a65e57c71056469b1af427d4c3d4d36e938
bhakti.patil <bhakti.patil@aniruddhagps.com>
2024-05-05 16:08:22 +0530

    saving data in database through routes

---
+++ backend/routes/auth.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const router = express.Router();
+const User=require('../models/User')
+
+// const User=
+
+// Create a usser using POST '/api/auth' Doessnt require the auth
+router.post('/',(req,res)=>{
+   console.log(req.body);
+   const user=User(req.body);
+   user.save();
+    res.send(req.body);
+    // res.json([])
+})
+
+module.exports=router

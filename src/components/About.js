5ff75d13fd1f6a973a6f63432f5a02a7d176fa9a
bhakti.patil <bhakti.patil@aniruddhagps.com>
2024-06-02 22:15:50 +0530

    added navbar and form and also use use location hooks (also Navlink method)

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,16 @@
-import React, { useContext, useEffect } from 'react'
-import noteContext from '../context/notes/noteContext'
+// import React, { useContext, useEffect } from 'react'
+// import noteContext from '../context/notes/noteContext'
+import React from "react"
 const About = () => {
-  const a = useContext(noteContext);
-  useEffect(() => {
-    a.update();
-    // eslint-disable-next-line
-  }, [])
+  // const a = useContext(noteContext);
+  // useEffect(() => {
+  //   a.update();
+  //   // eslint-disable-next-line
+  // }, [])
   
   return (
     <div>
-      This is about {a.state.name} and she is in class {a.state.class}
+      This is about page
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,31 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
+//   let location = useLocation();
+
+//  useEffect(() => {
+//    console.log(location.pathname)
+//   }, [location]);
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
     <div className="container-fluid">
-      <Link className="navbar-brand" to="/">Navbar</Link>
+      <Link className="navbar-brand" to="/">iNoteBook</Link>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        {/* <li className="nav-item">
+            <NavLink className={`nav-link ${location.pathname==="/"?"active":""}`} aria-current="page" to="/">Home</NavLink>
+          </li> */
+          // Had done this using useLocation Hook logic
+          } 
           <li className="nav-item">
-            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+            <NavLink className={({ isActive}) => isActive ? "nav-link active" : "nav-link"}aria-current="page" to="/">Home</NavLink>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/about">About</Link>
+            <NavLink className={({ isActive}) => isActive ? "nav-link active" : "nav-link"} to="/about">About</NavLink>
           </li>
           
           
